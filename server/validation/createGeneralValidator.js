407107cece25fddebdf18ce703b71c61182f34bb
samaorahno <samabos007@gmail.com>
2018-12-03 06:24:02 +0100

    [chore #162368851] Unit-test-for-api-endpoint

--- server/validation/createGeneralValidator.js
+++ server/validation/createGeneralValidator.js
@@ -1,9 +1,7 @@
 class createGeneralValidator {
   static validateString(string) {
     if (!string || string === undefined || string.toString().trim() === '' || typeof string !== 'string') return false;
-    if (string.lenght < 3 || string.lenght > 500) return false;
-    const stringAllowed = /^[a-zA-Z-'\s\d]+$/;
-    if (!string.match(stringAllowed)) return false;
+    if (string.length < 3 || string.length > 500) return false;
     return true;
   }
 
