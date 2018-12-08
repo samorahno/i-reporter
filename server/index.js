c61086764e90bc0f618796e5e18a9d880efac871
samaorahno <samabos007@gmail.com>
2018-12-08 12:13:34 +0100

    chore(packageJson): Package json file update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import redFlagsRoute from './routes/index';
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
@@ -14,8 +16,12 @@ app.get('/api/v1', (req, res) => res.status(200).send({
 app.use('/api/v1/red-flags', redFlagsRoute);
 
 app.get('*', (req, res) => res.status(404).send({ message: 'Page not found. Please visit /api/v1' }));
-const port = process.env.port || 3000;
-app.listen(port, () => console.log(`app running on port ${port}`));
+/* const port = process.env.port || 3000;
+app.listen(port, () => console.log(`app running on port ${port}`)); */
+
+const port = 9000;
+
+app.listen(process.env.PORT || `${port}`, () => console.log(`Server started at localhost ${port}`));
 
 
 export default app;
